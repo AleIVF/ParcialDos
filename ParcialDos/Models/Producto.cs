60194c8e3793092bb02b22d1890a054aec82ce45
av394 <av394@IXCHEL2507>
2024-10-21 18:57:13 -0600

    Agregar archivos de proyecto.

---
+++ ParcialDos/Models/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParcialDos.Models
+{
+    public class Producto
+    {
+        [Required(ErrorMessage = "El número de inventario es obligatorio")]
+        [Range(1, int.MaxValue, ErrorMessage = "El número de inventario debe ser mayor a 0")]
+        public int? NumeroInventario { get; set; }
+
+        [Required(ErrorMessage = "El nombre no debe ser vacío")]
+        [StringLength(200, ErrorMessage = "El nombre no puede superar los 200 caracteres")]
+        public string? Nombre { get; set; }
+
+        [Required(ErrorMessage = "Debe seleccionar un departamento")]
+        [RegularExpression("Frutas|Verduras|Lácteos", ErrorMessage = "El departamento debe ser Frutas, Verduras o Lácteos")]
+        public string? Departamento { get; set; }
+    }
+}
